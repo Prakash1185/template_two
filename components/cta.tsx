ee76ef9d14af0d4268e80844cdaaf805c2b32c97
Prakash Kumar <pgcet601@gmail.com>
2026-05-01 00:52:51 +0530

    fix errors

--- components/cta.tsx
+++ components/cta.tsx
@@ -16,7 +16,7 @@ const container = {
     filter: "blur(0px)",
     transition: {
       duration: 1.2,
-      ease: [0.22, 1, 0.36, 1],
+      ease: [0.22, 1, 0.36, 1]  as const,
     },
   },
 };
@@ -33,7 +33,7 @@ const fadeUp = {
     filter: "blur(0px)",
     transition: {
       duration: 1,
-      ease: [0.22, 1, 0.36, 1],
+      ease: [0.22, 1, 0.36, 1]  as const,
     },
   },
 };
